70a5eea35f47b318fc3f4c8eaea13968b899167b
BerHidalgo <bernardo.hidalgo_castro@lcieducation.net>
2022-11-25 22:38:52 -0600

    Creacion de Entidades Enemigos
    Se creo entidades enemigos con sus caracteristicas

---
+++ Entidades/Enemigo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Enemigo
+    {
+        public string nombreEnemigo;
+        public int vidaEnemigo;
+        public int ataqueEnemigo;
+        public bool jefe;
+
+        public Enemigo(string pnombreEnemigo,int pvidaEnemigo,int pataqueEnemigo,bool pjefe)
+        {
+            nombreEnemigo = pnombreEnemigo;
+           vidaEnemigo = pvidaEnemigo;
+           ataqueEnemigo = pataqueEnemigo;
+           jefe = pjefe;
+
+        }
+
+    }
+}
