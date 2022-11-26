c52cfd4d201bcf10cdd513e5ca095372ef69f2a9
BerHidalgo <bernardo.hidalgo_castro@lcieducation.net>
2022-11-25 22:39:23 -0600

    Se creo entidades Habitacion
    Se creo entidades habitacion con sus caracteristicas

---
+++ Entidades/Habitacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Habitacion
+    {
+        public string nombre;
+        public int probabilidad; 
+        public bool cofre;
+
+        //Metodo Cosntructor
+        public Habitacion(string pnombre,int pprobabilidad,bool pcofre)
+        {
+            nombre = pnombre;
+            probabilidad = pprobabilidad;
+            cofre = pcofre;
+
+        }
+    }
+}
