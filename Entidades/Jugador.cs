e1e9869a4967dc5cbec84e1d6b94e5faafcebb80
BerHidalgo <bernardo.hidalgo_castro@lcieducation.net>
2022-11-25 22:40:05 -0600

    Cambio en entidad Jugador
    Se actualizo el tipo de clase en jugador

--- Entidades/Jugador.cs
+++ Entidades/Jugador.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    internal class Jugador
+    
+    public class Jugador
     {
+        //Propiedades
+        public int vida;
+        public int ataque;
+        public int defensa;
+        public int pociones;
+
+        //Metodo constructor
+        public Jugador()
+        {
+            vida = 50;
+            ataque = 5;
+            defensa = 2;
+            pociones = 3;
+
+        }
     }
 }
