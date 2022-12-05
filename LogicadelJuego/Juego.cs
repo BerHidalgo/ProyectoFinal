888bbd3bb542a9f5363de7a611f9a9a27a60e3c6
BerHidalgo <bernardo.hidalgo_castro@lcieducation.net>
2022-12-05 14:10:28 -0600

    Carga de Datos de Enemigos y Jefes
    Se escribieron las diferentes caracteristicas para los diferentes jefes y enemigos del juego

--- LogicadelJuego/Juego.cs
+++ LogicadelJuego/Juego.cs
@@ -11,6 +11,7 @@ namespace LogicadelJuego
     {
         //Propiedades
         private Habitacion[] masmorra;
+        //Crear indice para saber habitacion actual. Resetear indice si perdi
         private int numeroDeHabitacion;
         private Jugador marcjugador;
         private Enemigo[] enemigos;
@@ -25,7 +26,7 @@ namespace LogicadelJuego
             numeroDeHabitacion = 0;
             this.masmorra = new Habitacion[9];
             marcjugador = new Jugador();
-            enemigos = new Enemigo[20];
+            enemigos = new Enemigo[18];
             jefes = new Enemigo[3];
             CargarDatosHabitacion();
             CargarDatosEnemigos();
@@ -33,8 +34,8 @@ namespace LogicadelJuego
 
         }
 
-        //Crear indice para saber con cual enemigo/jefe estoy peleando.
-        //Crear indice para saber habitacion actual. Resetear indice si perdi
+        
+        
         //Aumentar el indice de la habitaciones cuando avanzo.
 
         //JUGADOR
@@ -118,6 +119,7 @@ namespace LogicadelJuego
         //Cargar datos
         //Calcular si es una habitacion de cofres.
         //Calcular si hay cofre.
+        //Crear indice para saber con cual enemigo/jefe estoy peleando.
         public void CargarDatosHabitacion()
         {
             masmorra[0] = new Habitacion("Piso 1 Habitacion 1", 5, false,false);
@@ -132,15 +134,35 @@ namespace LogicadelJuego
                
         }
         //Enemigo(string pnombreEnemigo,int pvidaEnemigo,int pataqueEnemigo,bool pjefe)
+        //20 Enemigos
         public void CargarDatosEnemigos()
         {
-
+            enemigos[0] = new Enemigo("Goblin Guerrero", 20, 2,false);
+            enemigos[1] = new Enemigo("Goblin Hechicero", 10, 10, false);
+            enemigos[2] = new Enemigo("Goblin Ladron", 10, 5, false);
+            enemigos[3] = new Enemigo("Slime", 7, 3, false);
+            enemigos[4] = new Enemigo("Zombie Guerrero", 20, 5, false);
+            enemigos[5] = new Enemigo("Necromancer", 15, 12, false);
+            enemigos[6] = new Enemigo("Esqueleto", 10, 5, false);
+            enemigos[7] = new Enemigo("Bandido Guerrero", 25, 5, false);
+            enemigos[8] = new Enemigo("Bandido Hechicero", 15, 15, false);
+            enemigos[9] = new Enemigo("Bandido", 10, 10, false);
+            enemigos[10] = new Enemigo("Slime Grande", 10, 5, false);
+            enemigos[11] = new Enemigo("Hombre Lagarto", 30, 8, false);
+            enemigos[12] = new Enemigo("Lagarto Hechizero", 20, 15, false);
+            enemigos[13] = new Enemigo("Goblin Arquero", 5, 5, false);
+            enemigos[14] = new Enemigo("Bandido Arquero", 5, 10, false);
+            enemigos[15] = new Enemigo("Zombie Arquero", 5, 8, false);
+            enemigos[16] = new Enemigo("Lagarto Arquero", 10, 20, false);
+            enemigos[17] = new Enemigo("Rata Grande", 2, 5, false);
 
         }
 
         public void CargarDatosJefes()
         {
-
+            jefes[0] = new Enemigo("Mousse el Rey Slime", 50, 5, true);
+            jefes[1] = new Enemigo("Dednot el Resurrector", 30, 20, true);
+            jefes[2] = new Enemigo("Dis'noot el Apostrofe", 100, 10, true);
 
         }
     }
